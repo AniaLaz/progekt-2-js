9c97618983dd246e994f00282b176943a0de6d28
AniaLaz <104758969+AniaLaz@users.noreply.github.com>
2022-10-31 14:46:21 +0000

    Deploying to gh-pages from @ AniaLaz/progekt-2-js@d9f6ba4337460ff5edb9d2ee0c56d038a1a50217 🚀

---
+++ index.0e40942c.js
@@ -0,0 +1,2 @@
+console.log((async function(e,o,c){return await axios.get("https://api.themoviedb.org/3/movie/550?api_key=5b1449ced393d87bc0a1ea0f9fb4bc3e")}));const e=document.querySelector(".search-form__input");console.log(e);
+//# sourceMappingURL=index.0e40942c.js.map
